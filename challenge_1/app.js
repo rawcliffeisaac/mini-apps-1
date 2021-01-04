78b42fd400f56add4de03f18a44878723b5fb391
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2021-01-04 11:45:19 -0800

    Creates on click commands for each box of the board and alternates pattern

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,3 +9,28 @@ let gameState = {
     0, 0, 0
   ]
 }
+
+let handlers = {
+  handleBoxClick: (boxClickEvent) => {
+    if (gameState.currentBoard[boxClickEvent.toElement.id] === 0) {
+      handlers.handleGameProceeds(boxClickEvent.toElement.id)
+    }
+  },
+  handleGameProceeds: (boxIndex) => {
+    if (gameState.nextToMove === 'X') {
+      gameState.currentBoard[boxIndex] = 'X'
+      gameState.nextToMove = 'O'
+    } else {
+      gameState.currentBoard[boxIndex] = 'O'
+      gameState.nextToMove = 'X'
+    }
+    console.log(gameState.currentBoard)
+  }
+}
+
+console.log(document.querySelectorAll('.box'))
+
+document.querySelectorAll('.box')
+  .forEach( box =>
+    box.addEventListener('click', handlers.handleBoxClick)
+  );
